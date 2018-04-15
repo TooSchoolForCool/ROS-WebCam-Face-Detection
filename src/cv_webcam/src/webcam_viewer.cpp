5eac3013141b16e5338a801dd918d27f89b6db21
TooSchoolForCool <zeyuz@outlook.com>
2018-04-15 15:57:28 -0700

    remove face detection in webcam viewer

--- src/cv_webcam/src/webcam_viewer.cpp
+++ src/cv_webcam/src/webcam_viewer.cpp
@@ -10,25 +10,6 @@ using namespace cv;
 
 #define FACE_CASCADE_PATH "/media/psf/ROS-WebCam-Face-Detection/src/cv_webcam/assets/haarcascade_frontalface_alt2.xml"
 
-void face_detection(Mat &frame)
-{
-    CascadeClassifier face_cascade;
-    if( !face_cascade.load(FACE_CASCADE_PATH) ) {
-        ROS_ERROR("[ERROR] Cannot load file: %s", FACE_CASCADE_PATH);
-        exit(1);
-    }
-
-    vector<Rect> faces;
-    Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
-
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-    for(size_t i = 0; i < faces.size(); i++) {
-        rectangle(frame, faces[i], CV_RGB(0, 255,0), 3);
-    }
-}
 
 void viewerCallback(const sensor_msgs::ImageConstPtr &msg)
 {
@@ -42,12 +23,10 @@ void viewerCallback(const sensor_msgs::ImageConstPtr &msg)
         return;
     }
 
-    Mat detected_frame(cv_img_ptr->image);
-    face_detection(detected_frame);
-
-    cv::imshow("webcam", detected_frame);
+    cv::imshow("webcam", cv_img_ptr->image);
     cv::waitKey(1);
 }
+
 
 int main(int argc, char **argv)
 {
