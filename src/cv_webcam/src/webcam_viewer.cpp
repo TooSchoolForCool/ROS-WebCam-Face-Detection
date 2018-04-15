3c7711737ad94357f02b9fd01c4a2cddd208d4f9
TooSchoolForCool <zeyuz@outlook.com>
2018-04-15 11:57:35 -0700

    Add face detection in webcam_viewer

--- src/cv_webcam/src/webcam_viewer.cpp
+++ src/cv_webcam/src/webcam_viewer.cpp
@@ -1,8 +1,34 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <cv_bridge/cv_bridge.h>
+
+using namespace std;
+using namespace cv;
+
+#define FACE_CASCADE_PATH "/media/psf/ROS-WebCam-Face-Detection/src/cv_webcam/assets/haarcascade_frontalface_alt2.xml"
+
+void face_detection(Mat &frame)
+{
+    CascadeClassifier face_cascade;
+    if( !face_cascade.load(FACE_CASCADE_PATH) ) {
+        ROS_ERROR("[ERROR] Cannot load file: %s", FACE_CASCADE_PATH);
+        exit(1);
+    }
+
+    vector<Rect> faces;
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+    for(size_t i = 0; i < faces.size(); i++) {
+        rectangle(frame, faces[i], CV_RGB(0, 255,0), 3);
+    }
+}
 
 void viewerCallback(const sensor_msgs::ImageConstPtr &msg)
 {
@@ -14,9 +40,12 @@ void viewerCallback(const sensor_msgs::ImageConstPtr &msg)
     catch (cv_bridge::Exception& e) {  
         ROS_ERROR("[ERROR] cv_bridge exception: %s", e.what());  
         return;
-    } 
+    }
 
-    cv::imshow("webcam", cv_img_ptr->image);
+    Mat detected_frame(cv_img_ptr->image);
+    face_detection(detected_frame);
+
+    cv::imshow("webcam", detected_frame);
     cv::waitKey(1);
 }
 
